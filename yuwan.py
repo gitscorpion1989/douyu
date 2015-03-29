6f6e228877a22aaf07d04baff4ec6c23d91bc248
gitscorpion1989 <54liuyang2006@gmail.com>
2015-03-29 18:27:14 +0200

    2

--- yuwan.py
+++ yuwan.py
@@ -4,6 +4,10 @@ from selenium.webdriver.common.keys import Keys
 import time
 import json
 import operator
+import requests
+import pickle
+import urllib2
+from bs4 import BeautifulSoup
 
 start_date = "2015-01-01"
 end_date = "2015-03-22"
@@ -11,14 +15,12 @@ end_date = "2015-03-22"
 driver = webdriver.Firefox()
 driver.get("http://www.douyutv.com/member/login")
 
-#usr = driver.find_element_by_name("username")
-#usr.send_keys("清凈")
-#usr.send_keys(Keys.RETURN)
-
-input('name finished! ')
+usr = driver.find_element_by_name("username")
+usr.send_keys("scorpion1989")
+usr.send_keys(Keys.RETURN)
 
 pwd = driver.find_element_by_name("password")
-pwd.send_keys("*****")
+pwd.send_keys("54liuyang")
 pwd.send_keys(Keys.RETURN)
 
 cap = driver.find_element_by_name("captcha_word")
@@ -30,23 +32,46 @@ cap.send_keys(Keys.RETURN)
 
 time.sleep(2)
 
-cookie = {'name' : 'foo', 'value' : 'bar'}
-driver.add_cookie(cookie)
-driver.get_cookies()
-
 yuwan_dic = {}
 #http://www.douyutv.com/room/my/profit?time1=2015-01-01&time2=2015-03-22&seltype=&page=1
+#driver.get("http://www.douyutv.com/room/my/profit?time1=2015-01-01&time2=2015-03-22&seltype=&page=1")
 
-url_profit = "http://www.douyutv.com/room/my/profit?"
+#driver2 = webdriver.Firefox()
+
+#driver2.get("http://www.zhihu.com/")
+#driver2.delete_all_cookies()
+
+cookies = {}
+for cookie in driver.get_cookies():
+	 cookies[cookie["name"]]=cookie["value"]
+
+
+r = requests.get("http://www.douyutv.com/room/my/profit?time1=2015-01-01&time2=2015-03-22&seltype=&page=1", cookies = cookies)
+soup = BeautifulSoup(r.text)
+print r.text
+
+for table in soup.find_all('tr', {'class': "row01"}):
+	print table.find_all('td', text=True)[3].text
+#text = soup.prettify()
+#target = open ("test", 'a')
+#target.write(text.encode('utf-8').strip())
+
+#print "driver2"
+#print driver2.get_cookies()
+
+#driver2.get("http://www.zhihu.com/")
+
+#print "driver2"
+#print driver2.get_cookies()
+"""url_profit = "http://www.douyutv.com/room/my/profit?"
 url_time = "time1=" + start_date + "&time2=" + end_date + "&seltype=&"
 url_page = "page=" 
 
 #driver.get(url_profit + url_time + url_page + str(1))
 #total_page = (driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div[2]/p/span[2]")).get_attribute('innerHTML')
 #print total_page
-driver.set_page_load_timeout(10)
 
-for i in range(1,338):
+for i in range(1,2):
 	try:
 		driver.get(url_profit + url_time + url_page + str(i))
 	except:
@@ -63,3 +88,4 @@ for i in range(1,338):
 sorted_dic = sorted(yuwan_dic.items(), key=operator.itemgetter(1), reverse=True)
 
 print json.dumps(sorted_dic, encoding="UTF-8", ensure_ascii=False)
+"""
